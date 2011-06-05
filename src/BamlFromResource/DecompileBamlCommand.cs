9275ff44e6e62688373d4558a7b7477c2aa3da44
Cristian Prieto <me@cprieto.com>
2011-06-05 16:52:06 +1000

    Added xaml viewer window

--- src/BamlFromResource/DecompileBamlCommand.cs
+++ src/BamlFromResource/DecompileBamlCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Reflector.BamlViewer;
@@ -15,6 +16,14 @@ namespace Cprieto.DotPeek
             if (items ==  null || items.Count == 0) return;
 
             var resource = items[0] as Resource;
+            var window = new Window
+                             {
+                                 Content = new XamlViewer(),
+                                 Title = string.Format("Xaml source code for {0}", resource.Name),
+                                 ShowInTaskbar = false,
+                                 ResizeMode = ResizeMode.NoResize
+                             };
+            window.ShowDialog();
         }
 
         public bool CanExecute(object parameter)
