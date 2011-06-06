07ccbc0c742c39f2581b148ff971562b31c6bfd5
Cristian Prieto <me@cprieto.com>
2011-06-06 18:02:38 +1000

    Added word wrap support and fixed document display issue

--- src/BamlFromResource/XamlViewer.xaml.cs
+++ src/BamlFromResource/XamlViewer.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 using ActiproSoftware.Text;
 using ActiproSoftware.Text.Implementation;
+using ActiproSoftware.Windows.Controls.SyntaxEditor;
 
 namespace Cprieto.DotPeek
 {
@@ -25,8 +14,10 @@ namespace Cprieto.DotPeek
         {
             InitializeComponent();
             xmlEditor.Document.Language = GetEmbeddedLanguageSyntax();
+            xmlEditor.CanScrollPastDocumentEnd = false;
             xmlEditor.Document.IsReadOnly = true;
             xmlEditor.IsLineNumberMarginVisible = true;
+            xmlEditor.WordWrapMode = WordWrapMode.Word;
         }
 
         private ISyntaxLanguage GetEmbeddedLanguageSyntax()
